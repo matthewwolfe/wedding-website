89e1808bc038552ee69dca4c28df8f25ebc0690c
Matthew Wolfe <matthew.wolfe2@gmail.com>
2021-10-27 20:20:44 -0400

    build

--- docs/_next/static/7-myeO6iH181B0L-XwGum/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6af023a7e4d7fb9637c5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

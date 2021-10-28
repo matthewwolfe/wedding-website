0ed06e88d74b4a0fa38175d79d7f9ad86542a5a4
Matthew Wolfe <matthew.wolfe2@gmail.com>
2021-10-27 20:20:13 -0400

    changes to home page

--- src/components/Home/HomePage/HomePage.tsx
+++ src/components/Home/HomePage/HomePage.tsx
@@ -4,56 +4,111 @@ import { Flex } from 'components/Flex';
 
 function HomePage() {
   return (
-    <Flex sx={{ backgroundColor: '#f3e0da', flexDirection: { xs: 'column', lg: 'row' } }} width={1}>
+    <>
       <Flex
-        sx={{
-          px: { xs: 4, md: 0 },
-          flexDirection: 'column',
-          justifyContent: 'center',
-          height: { xs: 'initial', lg: '100vh' },
-          minHeight: 360,
-          color: '#a44812',
-          textAlign: 'center',
-          width: { xs: 1, lg: 0.6 },
-        }}
+        sx={{ backgroundColor: '#f3e0da', flexDirection: { xs: 'column', lg: 'row' } }}
+        width={1}
       >
         <Flex
           sx={{
-            mb: 4,
+            px: { xs: 4, md: 0 },
+            flexDirection: 'column',
             justifyContent: 'center',
-            width: 1,
+            height: { xs: 'initial', lg: '100vh' },
+            minHeight: 360,
+            color: '#a44812',
+            textAlign: 'center',
+            width: { xs: 1, lg: 0.6 },
           }}
         >
-          <Typography variant="h1">Save the date</Typography>
+          <Flex
+            sx={{
+              mb: 4,
+              justifyContent: 'center',
+              width: 1,
+            }}
+          >
+            <Typography variant="h1">Save the date</Typography>
+          </Flex>
+
+          <Typography variant="h2">
+            <Box display={{ xs: 'block', lg: 'inline' }}>April 8th, 2022</Box>{' '}
+            <Box display={{ xs: 'none', lg: 'inline' }}>|</Box> Rockville, MD
+          </Typography>
+
+          <Typography variant="h4">Formal Inviation to Follow.</Typography>
         </Flex>
 
-        <Typography variant="h2">
-          <Box display={{ xs: 'block', lg: 'inline' }}>April 8th, 2022</Box>{' '}
-          <Box display={{ xs: 'none', lg: 'inline' }}>|</Box> Rockville, MD
-        </Typography>
-
-        <Typography variant="h4">Formal Inviation to Follow.</Typography>
+        <Flex
+          sx={{
+            justifyContent: 'center',
+            width: { xs: 1, lg: 0.4 },
+          }}
+          justifyContent="center"
+        >
+          <Box
+            sx={{
+              height: { xs: 'auto', lg: '100vh' },
+              pt: { xs: 4, lg: 16 },
+              px: { xs: 8, sm: 24, md: 40, lg: 4 },
+            }}
+          >
+            {/* eslint-disable-next-line */}
+            <img alt="Holding hands" src="/images/holding-hands.png" height="100%" width="100%" />
+          </Box>
+        </Flex>
       </Flex>
 
       <Flex
-        sx={{
-          justifyContent: 'center',
-          width: { xs: 1, lg: 0.4 },
-        }}
-        justifyContent="center"
+        sx={{ backgroundColor: '#f7f0ee', flexDirection: { xs: 'column', lg: 'row' } }}
+        width={1}
       >
-        <Box
+        <Flex
           sx={{
-            height: { xs: 'auto', lg: '100vh' },
-            pt: { xs: 4, lg: 16 },
-            px: { xs: 8, sm: 24, md: 40, lg: 4 },
+            justifyContent: 'center',
+            order: { xs: 2, lg: 1 },
+            width: { xs: 1, lg: 0.4 },
+          }}
+          justifyContent="center"
+        >
+          <Box
+            sx={{
+              height: { xs: 'auto', lg: '100vh' },
+              px: { xs: 8, sm: 24, md: 40, lg: 4 },
+            }}
+          >
+            {/* eslint-disable-next-line */}
+            <img alt="Holding hands" src="/images/hugging.png" height="100%" width="100%" />
+          </Box>
+        </Flex>
+
+        <Flex
+          sx={{
+            px: { xs: 4, md: 0 },
+            flexDirection: 'column',
+            justifyContent: 'center',
+            height: { xs: 'initial', lg: '100vh' },
+            minHeight: 360,
+            color: '#a44812',
+            textAlign: 'center',
+            order: { xs: 1, lg: 2 },
+            width: { xs: 1, lg: 0.6 },
           }}
         >
-          {/* eslint-disable-next-line */}
-          <img alt="Holding hands" src="/images/holding-hands-lg.png" height="100%" width="100%" />
-        </Box>
+          <Flex
+            sx={{
+              mb: 4,
+              justifyContent: 'center',
+              width: 1,
+            }}
+          >
+            <Typography variant="h1">Details</Typography>
+          </Flex>
+
+          <Typography variant="h2">coming soon to this website...</Typography>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
 

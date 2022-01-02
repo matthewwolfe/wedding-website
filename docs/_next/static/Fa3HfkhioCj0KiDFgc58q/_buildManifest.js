c06cb8b8607856e716fcf75082ba8d6300b44842
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-01-02 16:51:10 -0500

    test deployment invites

--- docs/_next/static/Fa3HfkhioCj0KiDFgc58q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c787bc0d5819df74b2e3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

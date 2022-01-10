196bf0d1f636aef41bb1587d15322e20f245568f
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-01-09 20:21:46 -0500

    fix invalid base path

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-    };
-  }
-
-  return {
-    basePath: '/wedding-website',
-    reactStrictMode: true,
-  };
-};
+module.exports = (phase) => ({
+  reactStrictMode: true,
+});

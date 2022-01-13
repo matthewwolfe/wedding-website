7604e9a6b8434f55ba827ab22107208f1696434a
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-01-13 15:56:07 -0500

    change name

--- pages/invite/daniela-and-chris.tsx
+++ pages/invite/daniela-and-chris.tsx
@@ -1,7 +1,7 @@
 import { InvitePage } from 'components/Invites';
 
 function Invite() {
-  return <InvitePage name="Daniela J. and Partner" rsvpCount={2} />;
+  return <InvitePage name="Daniela J. and Christopher G." rsvpCount={2} />;
 }
 
 export default Invite;

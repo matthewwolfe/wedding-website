7aa5950f224163954c1df942c6fcecc798cc0cbc
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-01-18 18:09:49 -0500

    add new pages

---
+++ pages/invite/susie-and-trenton.tsx
@@ -0,0 +1,7 @@
+import { InvitePage } from 'components/Invites';
+
+function Invite() {
+  return <InvitePage name="Susie and Trenton" rsvpCount={2} />;
+}
+
+export default Invite;
---
+++ pages/invite/vivian-andalis.tsx
@@ -0,0 +1,7 @@
+import { InvitePage } from 'components/Invites';
+
+function Invite() {
+  return <InvitePage name="Vivian Andalis" rsvpCount={1} />;
+}
+
+export default Invite;

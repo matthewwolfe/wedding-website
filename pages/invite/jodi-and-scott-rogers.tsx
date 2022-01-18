ce0b6ff04b2899035ae894b92940a0c7bade5f44
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-01-18 18:16:30 -0500

    fix name

--- pages/invite/jodi-and-scott-rogers.tsx
+++ pages/invite/jodi-and-scott-rogers.tsx
@@ -1,7 +1,7 @@
 import { InvitePage } from 'components/Invites';
 
 function Invite() {
-  return <InvitePage name="Scott and Jody Rogers" rsvpCount={2} />;
+  return <InvitePage name="Scott and Jodi Rogers" rsvpCount={2} />;
 }
 
 export default Invite;
